805904d8e15d86727041f08357535c105c97dca3
Eleonora Mometti <eleonora.mometti731@gmail.com>
2026-02-17 20:37:02 +0100

    Initial commit Birthday card

---
+++ birthday-card.py
@@ -0,0 +1,19 @@
+#Information
+recipient = "Kurt"
+year_of_birth = 1995
+personalized_msg = "Best wishes for this special day! Let's toast and laugh together soon, as always."
+sender = "Eleonora"
+
+#Age calculation
+current_year= 2026
+recipient_age = current_year - year_of_birth
+
+#Birthday card
+print()
+print(recipient, ", let's celebrate your", recipient_age, " years of awesomeness!")
+print("Wishing you a day filled with joy and laughter as you turn", recipient_age)
+print()
+print(personalized_msg)
+print()
+print("With love and best wishes,")
+print(sender)
---
+++ main.py
@@ -0,0 +1,14 @@
+msg = " hello "
+msg1 = "goodbye "
+
+print(msg + msg1)
+print(msg + msg1)
+print(msg)
+print(msg + "vaffanculo " + msg1)
+
+msg = "-----"
+a = 1
+b = 2
+print("-----" + a + b)
+
+
